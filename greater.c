de6d98024f932e52e2562da890838065bd7848f9
ubuntu <aitechmanml@gmail.com>
2024-09-19 09:21:56 +0530

    demos

---
+++ greater.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+  int i, j;
+  printf("Enter two integers: ");
+  scanf("%d %d", &i, &j);
+
+  if (i > j)
+    printf("%d is greater than %d\n", i, j);
+  else if (j > i)
+    printf("%d is greater than %d\n", j, i);
+  else {
+    printf("i and j are equal\n");
+  }
+
+  return 0;
+}
