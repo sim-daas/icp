56c02ce8163fbc128875fc26027f681b2ec5d241
ubuntu <aitechmanml@gmail.com>
2024-11-13 11:39:23 +0530

    hello

--- assgn.c
+++ assgn.c
@@ -1,22 +1,45 @@
 #include <stdio.h>
 
+void sortDescending(int array[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if (array[i] < array[j]) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+}
+
 int main() {
-    int numberOfElements;
+    int size;
 
-    printf("Enter the number of elements: ");
-    scanf("%d", &numberOfElements);
+    printf("Enter the size of the arrays: ");
+    scanf("%d", &size);
 
-    int sourceArray[numberOfElements], destinationArray[numberOfElements];
+    int firstArray[size], secondArray[size], mergedArray[2 * size];
 
-    printf("Enter %d elements for the source array:\n", numberOfElements);
-    for (int index = 0; index < numberOfElements; index++) {
-        scanf("%d", &sourceArray[index]);
-        destinationArray[index] = sourceArray[index];
+    printf("Enter %d elements for the first array:\n", size);
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &firstArray[i]);
     }
 
-    printf("Elements in the destination array are:\n");
-    for (int index = 0; index < numberOfElements; index++) {
-        printf("%d ", destinationArray[index]);
+    printf("Enter %d elements for the second array:\n", size);
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &secondArray[i]);
+    }
+
+    for (int i = 0; i < size; i++) {
+        mergedArray[i] = firstArray[i];
+        mergedArray[size + i] = secondArray[i];
+    }
+
+    sortDescending(mergedArray, 2 * size);
+
+    printf("Merged array in descending order:\n");
+    for (int i = 0; i < 2 * size; i++) {
+        printf("%d ", mergedArray[i]);
     }
 
     return 0;
